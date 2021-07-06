62e8960e85a90ea6d31a43c3287ec44561923d3c
syazilijuhari <74142207+syazilijuhari@users.noreply.github.com>
2021-07-06 22:10:21 +0800

    Update

--- myPT4/ajax/search.php
+++ myPT4/ajax/search.php
@@ -15,23 +15,23 @@ if (isset($_GET['search'])) {
 
     try {
         //search each 3 keywords or search 3 keyword at once
-    //     if(count($data)==1){
-    //         $stmt = $db->prepare("SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE ? OR {$field[1]} LIKE ? OR {$field[2]} LIKE ?");
-    //         $stmt->execute(["%{$search}%","%{$search}%", "%{$search}%"]);
-    //     }
+        if(count($data)==1){
+            $stmt = $db->prepare("SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE ? OR {$field[1]} LIKE ? OR {$field[2]} LIKE ?");
+            $stmt->execute(["%{$search}%","%{$search}%", "%{$search}%"]);
+        }
 
-    //    elseif(count($data)==3){
-    //         $stmt = $db->prepare("SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE ? AND {$field[1]} LIKE ? AND {$field[2]} LIKE ?");
-    //         $stmt->execute(["%{$name}%","%{$region}%", "%{$era}%"]);
-    //     }
+       elseif(count($data)==3){
+            $stmt = $db->prepare("SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE ? AND {$field[1]} LIKE ? AND {$field[2]} LIKE ?");
+            $stmt->execute(["%{$name}%","%{$region}%", "%{$era}%"]);
+        }
 
         //search any keywords from 3 attributes
-        $queries = array();
-        foreach($data as $dat){
-            $queries[] = "SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE '%{$dat}%' OR {$field[1]} LIKE '%{$dat}%' OR {$field[2]} LIKE '%{$dat}%'";
-        }
-        $sql = implode(' UNION ',$queries);
-        $stmt = $db->prepare($sql);
+        // $queries = array();
+        // foreach($data as $dat){
+        //     $queries[] = "SELECT * FROM `tbl_products_a173630_pt2` WHERE {$field[0]} LIKE '%{$dat}%' OR {$field[1]} LIKE '%{$dat}%' OR {$field[2]} LIKE '%{$dat}%'";
+        // }
+        // $sql = implode(' UNION ',$queries);
+        // $stmt = $db->prepare($sql);
         
         $stmt->execute();
         $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
